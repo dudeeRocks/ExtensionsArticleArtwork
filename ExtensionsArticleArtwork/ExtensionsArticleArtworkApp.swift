9cbfb1e41c27d49524484c31fe547dd30c7e1db8
David Katsman <kadamc90@gmail.com>
2024-09-17 19:18:56 +0300

    Initial Commit

---
+++ ExtensionsArticleArtwork/ExtensionsArticleArtworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtensionsArticleArtworkApp.swift
+//  ExtensionsArticleArtwork
+//
+//  Created by David Katsman on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExtensionsArticleArtworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
